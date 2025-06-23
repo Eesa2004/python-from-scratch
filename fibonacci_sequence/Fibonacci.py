3324076d312f0960c5694ffbcf1680b22f6f1b11
Eesa Hassan <eesa.engineer@gmail.com>
2025-06-24 00:44:33 +0300

    sort loose py files into subfolders

--- fibonacci_sequence/Fibonacci.py
+++ fibonacci_sequence/Fibonacci.py
@@ -2,8 +2,8 @@ sum_fibonacci = 0
 num1 = 1
 num2 = 2
 fibonacci_total = 0
-fibonacci_list = [1,2]
-even_num = [0,2,4,6,8]
+fibonacci_list = [1, 2]
+even_num = [0, 2, 4, 6, 8]
 
 while num2 < 4000000:
     fibonacci_total = num1 + num2
@@ -12,16 +12,16 @@ while num2 < 4000000:
     fibonacci_list.append(num2)
 
 for num in fibonacci_list:
-    if num%2 == 0:
+    if num % 2 == 0:
         sum_fibonacci += num
 
     # str_num = str(num)
     # for i in even_num:
     #     if str_num[-1] == str(i):
     #         sum_fibonacci += num
-            #print(num)
+    # print(num)
 
 
 print(fibonacci_total)
-#print(fibonacci_list)
+print(fibonacci_list)
 print(sum_fibonacci)
